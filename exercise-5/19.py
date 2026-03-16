ada25a4c2c918d50730563480574b2325d1b5c14
jaishubhakanth999 <jaishubhakanth999@gmail.com>
2026-03-16 10:40:13 +0530

    up

--- exercise-5/19.py
+++ exercise-5/19.py
@@ -5,7 +5,7 @@ weave([1,2,3],[4,5,6]) -> [1,2,3,4,5,6] '''
 def weave(l , m):
     len1 = len(l)
     len2 = len(m)
-    if len1 == len2:
+    if len1 == len2 :
         for i in range(len1):
             l.append(m[i])
         return f"list after weaving : {l}"
